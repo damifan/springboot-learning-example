4668f09dfda77f75b52f4f5e16c82d456b63eee9
wanglei <wanglei59@jd.com>
2017-08-30 15:42:18 +0800

    spring-boot-kafka

---
+++ springboot-mybatis-redis-kafka/src/main/java/org/spring/springboot/Application.java
@@ -0,0 +1,30 @@
+package org.spring.springboot;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ * <p>
+ * Created by bysocket on 16/4/26.
+ */
+// Spring Boot 应用的标识
+@SpringBootApplication
+// mapper 接口类扫描包配置
+@MapperScan("org.spring.springboot.dao")
+public class Application {
+    public static Logger logger = LogManager.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        long t = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            logger.info("inf1o123" + t + "info" + i);
+        }
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(Application.class, args);
+    }
+}
